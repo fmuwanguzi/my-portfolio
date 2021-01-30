27131ce8a454beea1224dd3d2fe3611966aeceba
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2021-01-30 13:58:09 -0500

    [Past projects] card set up with links

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Image } from 'react-bootstrap'
 
 const About = () => {
     return(
         <div>
+            <Image src="holder.js/171x180" roundedCircle />
             About page
         </div>
     )
--- src/pages/PastProjects.js
+++ src/pages/PastProjects.js
@@ -1,59 +1,64 @@
 import React from 'react'
-import { Card, Button } from 'react-bootstrap'
+import { Card, Button, CardColumns } from 'react-bootstrap'
 
 const PastProjects = () => {
     return(
-        <div>
-            <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
+        
+            <CardColumns>
+            <Card  bg={'info'}style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="/images/snake.png" />
             <Card.Body>
                 <Card.Title>Snake Head Game</Card.Title>
                 <Card.Text>
                 Project built with HTML canvas and javscript.
                 </Card.Text>
-                <Button variant="primary">Deployed Link</Button>
-                <Button variant="primary">Github Link</Button>
+                <a href="https://fmuwanguzi.github.io/"><Button variant="dark">Deployed Link</Button> </a> 
+                <a href="https://github.com/fmuwanguzi/Snake-game"><Button variant="dark">Github Link</Button> </a>
             </Card.Body>
             </Card>
 
-            <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
+
+            <Card  bg={'info'}style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="/images/weather.png" />
             <Card.Body>
                 <Card.Title>My Simple weather app</Card.Title>
                 <Card.Text>
-                Project built with node.js, express and the openweathermap API
+                Project built with authenitaction, node.js, express and the openweathermap API
                 </Card.Text>
-                <Button variant="primary">Deployed Link</Button>
-                <Button variant="primary">Github Link</Button>
+                <a href="https://my-weather-app.herokuapp.com/"><Button variant="dark">Deployed Link</Button> </a> 
+                <a href="https://github.com/fmuwanguzi/weather-app"><Button variant="dark">Github Link</Button> </a>
             </Card.Body>
             </Card>
 
-            <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
+
+            <Card  bg={'info'}style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="/images/fit.png" />
             <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>Workout and nutrition</Card.Title>
                 <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
+                Front end built with React.js and backend built with mongoose(hosted on mongoDB)
                 </Card.Text>
-                <Button variant="primary">Deployed Link</Button>
-                <Button variant="primary">Github Link</Button>
+                <a href="https://generalfit.herokuapp.com/"><Button variant="dark">Deployed Link</Button> </a> 
+                <a href="https://github.com/fmuwanguzi/fitness_app_client"><Button variant="dark">Github Link</Button> </a>
             </Card.Body>
             </Card>
 
-            <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
+
+            <Card  bg={'info'}style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="/images/ikick.png" />
             <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>Kick it(My social app)</Card.Title>
                 <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
+                Front end built with React.js and backend built with mongoose(hosted on mongoDB)
                 </Card.Text>
-                <Button variant="primary">Deployed Link</Button>
-                <Button variant="primary">Github Link</Button>
+                <a href="https://ikick.herokuapp.com/"><Button variant="dark">Deployed Link</Button> </a> 
+                <a href="https://github.com/fmuwanguzi/social_app_client"><Button variant="dark">Github Link</Button> </a>
             </Card.Body>
             </Card>
-        </div>
+
+
+            </CardColumns>
+        
     )
 }
 
